677a66b5634fe5b8b2b83987c80e1e6ae1d3ebd2
poojana <poojana1608@gmail.com>
2026-06-14 01:21:01 +0530

    Updated API URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+    baseURL: "https://urlify-backened-ycff.onrender.com/api"
+  
 });
 
 export default API;
