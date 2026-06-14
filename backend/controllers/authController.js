ef9e6435adad232890c73f500736846857441deb
poojana <poojana1608@gmail.com>
2026-06-14 13:33:59 +0530

    Fixed auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,7 +12,7 @@ const registerUser = async (req, res) => {
 
     if (existingUser) {
       return res.status(400).json({
-        message: "User already exists"
+        message: "Email already registered",
       });
     }
 
@@ -23,17 +23,24 @@ const registerUser = async (req, res) => {
     const user = await User.create({
       name,
       email,
-      password: hashedPassword
+      password: hashedPassword,
     });
 
     res.status(201).json({
       message: "User Registered Successfully",
-      user
+      user,
     });
 
   } catch (error) {
+
+    if (error.code === 11000) {
+      return res.status(400).json({
+        message: "Email already registered",
+      });
+    }
+
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
 };
@@ -49,43 +56,46 @@ const loginUser = async (req, res) => {
 
     if (!user) {
       return res.status(404).json({
-        message: "User not found"
+        message: "User not found",
       });
     }
 
     // Compare Password
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
 
     if (!isMatch) {
       return res.status(401).json({
-        message: "Invalid Credentials"
+        message: "Invalid Credentials",
       });
     }
 
     // Generate Token
     const token = jwt.sign(
       {
-        id: user._id
+        id: user._id,
       },
       process.env.JWT_SECRET,
       {
-        expiresIn: "7d"
+        expiresIn: "7d",
       }
     );
 
     res.status(200).json({
       message: "Login Successful",
-      token
+      token,
     });
 
   } catch (error) {
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
 };
 
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
 };
