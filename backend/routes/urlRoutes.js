4b75977ed846bd7d58dd1eeae2c9a3518c9fac86
poojana <poojana1608@gmail.com>
2026-06-14 11:50:42 +0530

    Fixed url routes

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const {
   createShortUrl,
+  redirectUrl,
   getUserUrls,
   deleteUrl,
   getAnalytics
@@ -13,12 +14,16 @@ const protect = require("../middleware/authMiddleware");
 // Create Short URL
 router.post("/shorten", protect, createShortUrl);
 
-
 // Get All URLs of Logged-in User
 router.get("/myurls", protect, getUserUrls);
+
+// Analytics
 router.get("/analytics", protect, getAnalytics);
 
 // Delete URL
 router.delete("/:id", protect, deleteUrl);
+
+// Redirect Short URL
+router.get("/:shortCode", redirectUrl);
 
 module.exports = router;
